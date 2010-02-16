95c3746c37ad5a75f8f4ca07ff29e543075e905e
nicolassmith <nicolas.d.smith@gmail.com>
2010-02-15 20:49:31 -0500

    added blrmsseries.m

---
+++ blrmsseries.m
@@ -0,0 +1,21 @@
+function data_out = blrmsseries(data_in,BP,LP)
+% BLRMSSERIES
+% returns a time series Band Limited RMS calculated from input time series
+% 'data_in'
+%
+% usage: data_out = blrmsseries(data_in,BP,LP)
+%
+% data_in is input time series data, BP is dfilt object used for
+% bandpassing, LP is dfilt object used for low passing.
+
+% first bandpass the data using BP
+passed = filter(BP,data_in);
+
+% square
+squared = passed.^2;
+
+% low pass
+meaned = filter(LP,squared);
+
+% root
+data_out = sqrt(meaned);
