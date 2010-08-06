8a9c2359c2e8f15c708ebf88392f33265a60bdbb
nicolassmith <nicolas.d.smith@gmail.com>
2010-08-06 12:33:05 -0400

    colorlist update for SRSspec

--- SRSspec.m
+++ SRSspec.m
@@ -1,11 +1,12 @@
 function SRSspec(varargin)
 
-colorlist = {'k','r','b','g','c','m','y'};
+%colorlist = {'k','r','b','g','c','m','y'};
+colorlist = { [1 1 1], [1 0 0], [0 1 0], [0 0 1], [1 1 0], [.75 .25 .75], [0 1 1] };
 for j=1:length(varargin)
     M=varargin{j};
     %fldname = char(fieldnames(S));
     %M=getfield(S,fldname);
-    loglog(M(:,1),M(:,2),colorlist{mod(j,length(colorlist))+1})
+    loglog(M(:,1),M(:,2),'Color',colorlist{mod(j,length(colorlist))+1})
     hold on
 end
 grid on
