98d2cd64e5da45c7d9d2abc601a0ae514ba714a1
Nicolas Smith <nicolas.d.smith@gmail.com>
2014-12-04 19:59:42 -0800

    load cell array of files gives cell array of frds

--- SRSloadspec.m
+++ SRSloadspec.m
@@ -1,5 +1,14 @@
 function spec = SRSloadspec(filename)
 
+    if iscell(filename)
+        spec = {};
+        for name=filename
+           spec = [spec,{SRSloadspec(name{:})}]; %#ok<AGROW>
+        end
+        return;
+    end
+            
+
     M = SRSconvert(filename);
     
     spec = asddata(M(:,2),M(:,1));
