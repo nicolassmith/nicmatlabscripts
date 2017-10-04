e08b21b7732d8b9250128b5ba83714b4057088a5
Nicolas Smith <nicolas@planet.com>
2017-10-04 13:36:35 -0700

    Only apply linewidth=3 to the lines in the graph.

--- makeAxesPretty.m
+++ makeAxesPretty.m
@@ -13,7 +13,12 @@ function makeAxesPretty(haxes)
     % set fonts for labels and tick marks
     set([htitle,hxlabel,hylabel,gca],'FontSize',20,'LineWidth',3,'FontWeight','bold','FontName'   , 'Times')
     % set thick line widths for curves
-    set(get(haxes,'Children'),'LineWidth',3)
+    children = get(haxes,'Children');
+    isLine = zeros(1,length(children))==1;
+    for j = 1:length(children);
+        isLine(j) = strcmp(children(j).Type,'line');
+    end
+    set(children(isLine),'LineWidth',3)
     % white background
     set(get(haxes,'Parent'),'Color','White')
     set(gcf,'Renderer','painters');
